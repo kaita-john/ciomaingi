125dabfdd1c3ff9147dc54f120ac62ee104bb3c2
kaita <johnkaitadocuments@gmail.com>
2025-05-25 15:01:20 +0300

    Fix build and deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,15 @@ export default defineConfig({
     server: {
         allowedHosts: [
             '*',
-            '4383-41-215-50-90.ngrok-free.app', // Add your ngrok host here
+            '4383-41-215-50-90.ngrok-free.app',
             'b46d-41-215-50-90.ngrok-free.app',
-            'localhost', // Keep localhost if needed
-            '127.0.0.1', // Keep this for local development
+            'localhost',
+            '127.0.0.1',
         ],
     },
     plugins: [react()],
-})
+    base: '/CandleApp_FrontEnd_React/', // Matches GitHub Pages path
+    build: {
+        outDir: 'dist', // Ensure output directory is dist
+    },
+});
